16d9b4b79297e1061818089c4be45a5b8e3cbdb3
Andrew Gouin <andrew@gouin.io>
2023-01-17 21:15:52 -0700

    Remove ModuleName var

--- x/params/types/keys.go
+++ x/params/types/keys.go
@@ -1,14 +1,6 @@
 package types
 
-const (
-	// ModuleName defines the module name
-	ModuleName = "params"
-
-	// QuerierRoute defines the module's query routing key
-	QuerierRoute = ModuleName
-
-	AuthorityKey = "authority"
-)
+const AuthorityKey = "authority"
 
 func KeyPrefix(p string) []byte {
 	return []byte(p)
